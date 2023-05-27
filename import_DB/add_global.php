709e676d29a5112a75671859a820d3a8b21c594b
eweb-ossy <ossy2000@gmail.com>
2023-05-27 16:44:47 +0900

    fix: DB global

--- import_DB/add_global.php
+++ import_DB/add_global.php
@@ -6,29 +6,14 @@ $username = "dakoku_db_root";
 $password = "HyN8iK85rJW9";
 $database = 'global';
 
-// Connect to MySQL server
-mysql_connect($servername, $username, $password) or die('Error connecting to MySQL server: ' . mysql_error());
-// Select database
-mysql_select_db($database) or die('Error selecting MySQL database: ' . mysql_error());
+$conn = new PDO("mysql:host=$servername; dbname=$database", $username, $password);
 
-// Temporary variable, used to store current query
-$templine = '';
-// Read in entire file
-$lines = file($filename);
-// Loop through each line
-foreach ($lines as $line) {
-    // Skip it if it's a comment
-    if (substr($line, 0, 2) == '--' || $line == '') continue;
+$query = file_get_contents($filename);
 
-    // Add this line to the current segment
-    $templine .= $line;
-    // If it has a semicolon at the end, it's the end of the query
-    if (substr(trim($line), -1, 1) == ';') {
-        // Perform the query
-        mysql_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
-        // Reset temp variable to empty
-        $templine = '';
-    }
+$stmt = $conn->prepare($query);
+
+if ($stmt->execute()) {
+    echo "global DB Success";
+} else {
+    echo "Fail";
 }
-echo "Tables imported successfully";
-?>
